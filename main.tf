42e0d683bac39a3b71c05e73dfaecd70bfa65bfa
shaikharbaaz101 <shaikharbaaz101@gmail.com>
2025-01-26 19:13:24 +0530

    adding new files

--- main.tf
+++ main.tf
@@ -1,55 +1,21 @@
-terraform { 
-
-  required_providers { 
-
-    aws = { 
-
-      source  = "hashicorp/aws" 
-
-      version = "~> 4.16" 
-
-    } 
-
-  } 
-
-  
-
-  required_version = ">= 1.2.0" 
-
-} 
-
-  
-
-provider "aws" { 
-
-  region  = "ap-south-1" 
-
-  profile = "poppy" 
-
-} 
-
-  
-
 resource "aws_instance" "example_server" { 
-
-  ami           = "ami-00bb6a80f01f03502" 
-
-  instance_type = "t2.micro" 
-
-  
+  ami           = var.ami_id_1
+  instance_type = var.instancetype
 
   tags = { 
+    Name = var.instancename
+  } 
 
-    Name = "terraform1" 
+}
+
+
+resource "aws_instance" "example_server_1" { 
+  ami           = var.ami_id_2
+  instance_type = var.instancetype
+
+  tags = { 
+    Name = var.instancename
 
   } 
 
 } 
-
-  
-
-output "public_instance_ip" { 
-
-  value = aws_instance.example_server.public_ip
-
-}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id_1" {
+    type = string
+    default = "ami-00bb6a80f01f03502"
+}
+
+variable "ami_id_2" {
+    type = string
+    default = "ami-00bb6a80f01f03502"
+}
+
+variable "instancetype" {
+    type = string
+    default = "t2.micro"
+}
+
+variable "instancename" {
+    type = string
+    default = "testing_variable"
+}
